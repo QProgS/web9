88562e045aadfa98429e60ce29af04b6c04747e1
uuinnk <uuinnk>
2016-10-11 18:28:11 +0800

    fix

--- src/main/java/com/example/service/ArticleService.java
+++ src/main/java/com/example/service/ArticleService.java
@@ -6,6 +6,8 @@ import com.example.domain.Word;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Array;
@@ -28,15 +30,20 @@ public class ArticleService {
     public Article save(Article article){
         article.words = new HashSet<>();
         Set<String> list = new HashSet<>(Arrays.asList(article.content.split("\\W+")));
-
-        logger.info(list + " split list.");
+        
         for (String item : list) {
-        	List<String> wordForms = getWordForms(item);
+        	List<String> wordForms = getWordForms(item);        	
         	Word word = null;
         	for(String name : wordForms){
         		word = wordRepository.findByName(name);
         		if(word != null) break;
         	}            
+        	if(word == null){        		
+        		Page<Word> words = wordRepository.findByNameLike(wordForms.get(wordForms.size()-1)+"%", new PageRequest(0,1));
+        		logger.info("Try find " + wordForms.get(wordForms.size()-1) + " " + words.getContent());        		
+        		if(words.getContent().size() > 0)word = words.getContent().get(0);
+        	}
+        	
             if (word != null){
                 article.words.add(word);
             }
@@ -48,11 +55,12 @@ public class ArticleService {
     public Article findOne(long id) {
         Article article = articleRepository.findOne(id);        
 
-        List<String> list = new ArrayList<>(Arrays.asList(article.content.split("\\s")));
+        List<String> list = new ArrayList<>(Arrays.asList(article.content.split("\\s+")));
 
         StringBuilder sb = new StringBuilder();
 
         for (String item : list){
+        	if(item.trim().length() == 0)continue;
             long wordId = findItem(item,article.words);
             if(wordId != -1) {
                 sb.append(processWord(wordId,item));
@@ -67,11 +75,17 @@ public class ArticleService {
     }
 
     private long findItem(String item, Set<Word> words) {
-        for (Word word : words){
-            List<String> wordForms = getWordForms(item);
+    	List<String> wordForms = getWordForms(item);
+        for (Word word : words){            
             if(wordForms.contains(word.name)){
                 return word.getId();
             }
+        }
+        String lastItem = wordForms.get(wordForms.size()-1);
+        for (Word word : words){
+        	if(word.name.startsWith(lastItem)){
+        		return word.getId();
+        	}
         }
         return -1;
     }
