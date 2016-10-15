b12865a2df386c5a870760f9ab57962c10a0e267
QProgS <uu.innk@gmail.com>
2016-10-16 01:33:50 +0800

    word-popup v0.1

--- src/main/resources/static/js/word-popup.js
+++ src/main/resources/static/js/word-popup.js
@@ -1,21 +1,68 @@
+riot.mount('*');
+
 function ModalWord() {
     self = this;
+    var word = null;
+
+    self.setWord = function(word) {
+        if(self.word != null) self.word.removeClass('active');
+        self.word = word;
+        self.word.addClass('active');
+        self.word.parent().addClass("word-selected");
+    };
+    self.unsetWord = function() {
+        if(self.word != null) {
+            self.word.removeClass('active');
+            self.word.parent().removeClass("word-selected");
+        }
+        self.word = null;
+    };
+
     riot.observable(self)
 }
 var modalWord = new ModalWord();
 
-$(document).on("click", '.active-content span', function () {
-    var id = $(this).data("id");
+$(document).on("click", ".active-content span", function () {
+    var self = $(this);
+    var id = self.data("id");
+    if(id == undefined){console.error(".active-content span data-id - is undefined"); return;}
 
     $.getJSON( "/words/json/" + id, function( data ) {
-        modalWord.trigger('setWord', data);
+        if(data == null)console.error("/words/json/" + id + " is null");
+        else {
+            modalWord.trigger('setWord', data);
+            modalWord.setWord(self);
+            showWordPopup(self);
+        }
     });
 
 });
 
-riot.compile(function () {
-    riot.mount('*');
+function showWordPopup(word) {
+    $('#word-popup__overlay').css("display", "block");
+
+    $('#word-popup__block')
+        .css("top", word.offset().top - word.parent().offset().top + 19)
+        .css("position", "absolute")
+        .css("width",     word.parent().width())
+        .css("max-width", word.parent().width())
+        .css("display", "block");
+
+    $('#word-popup__block .arrow')
+        .css("left", word.offset().left + word.width()/2.0 - word.parent().offset().left);
+
+    var title = $('#word-popup__block .popover-title span');
+    title.css("left", word.offset().left + word.width()/2.0  - word.parent().offset().left
+        - title.width()/2.0);
+
+}
+
+$(document).on("click", "#word-popup__overlay", function () {
+    $('#word-popup__overlay').css("display", "none");
+    $('#word-popup__block').css("display", "none");
+    modalWord.unsetWord();
 });
+
 
 
 
--- src/main/resources/static/tags/word-popup.tag
+++ src/main/resources/static/tags/word-popup.tag
@@ -1,22 +1,25 @@
 <word-popup>
-    <h3>{word.name}</h3>
-    <ol if="{word.definitions}" >
-        <li each="{word.definitions}">{text}</li>
-    </ol>
+    <div id="word-popup__outer">
+        <div id="word-popup__overlay"></div>
+        <div id="word-popup__block" class="popover fade bottom in">
+            <div class="arrow"></div>
+            <h3 class="popover-title"><span>{word.name}</span></h3>
+            <div class="popover-content">
+                <ol if="{word.definitions}" >
+                    <li each="{word.definitions}">{text}</li>
+                </ol>
+            </div>
+        </div>
+    </div>
 
     <script>
         var self = this
 
         modalWord.on('setWord', function (word) {
-            console.log(word);
+            //console.log('on setWord', word);
             self.word = word;
             self.update()
         })
     </script>
 
-    <style>
-        ol{
-            font-size: 14px;
-        }
-    </style>
 </word-popup>
